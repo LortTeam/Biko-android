f388eb4ce519f0efcebd093c17e493766857f48e
snusmumriki <wdart1995@gmail.com>
2017-05-27 16:30:13 +0200

    1

---
+++ app/src/main/java/hu/pe/biko/biko/entity.kt
@@ -0,0 +1,9 @@
+package hu.pe.biko.biko
+
+data class Place(var name: String, var description: String, var address: String,
+                 var lat: Double, var lng: Double, var city: String, var country: String, var state: String)
+
+data class Route(var name: String, var description: String, var address: String,
+                 var city: String, var country: String, var state: String, var distance: Int, var time: Int)
+
+
