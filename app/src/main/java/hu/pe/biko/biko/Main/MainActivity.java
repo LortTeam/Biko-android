4a60b49fc46ed4741a6610648f0bfe3d807b1841
Лиза <elizaveta.barymova.00@mail.ru>
2017-05-27 15:41:47 +0200

    1

--- app/src/main/java/hu/pe/biko/biko/Main/MainActivity.java
+++ app/src/main/java/hu/pe/biko/biko/Main/MainActivity.java
@@ -1,4 +1,4 @@
-package hu.pe.biko.biko;
+package hu.pe.biko.biko.Main;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -6,6 +6,8 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
+
+import hu.pe.biko.biko.R;
 
 public class MainActivity extends AppCompatActivity {
 
