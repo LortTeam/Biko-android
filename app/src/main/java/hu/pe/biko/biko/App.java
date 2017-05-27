45a0ed80ff33131e3254a3f747b206deb74734ad
snusmumriki <wdart1995@gmail.com>
2017-05-27 19:40:46 +0200

    1

---
+++ app/src/main/java/hu/pe/biko/biko/App.java
@@ -0,0 +1,10 @@
+package hu.pe.biko.biko;
+
+import android.support.multidex.MultiDexApplication;
+
+/**
+ * Created by Galya Sheremetova on 27.05.2017.
+ */
+
+public class App extends MultiDexApplication {
+}
