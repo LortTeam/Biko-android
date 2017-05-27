111c361f74828d5aed4910f3ec22c01e696dcd94
Лиза <elizaveta.barymova.00@mail.ru>
2017-05-27 15:50:22 +0200

    1

--- app/src/main/java/hu/pe/biko/biko/Main/AccountFragment.java
+++ app/src/main/java/hu/pe/biko/biko/Main/AccountFragment.java
@@ -1,7 +1,6 @@
 package hu.pe.biko.biko.Main;
 
-import android.content.Context;
-import android.net.Uri;
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -11,10 +10,10 @@ import android.view.ViewGroup;
 import hu.pe.biko.biko.R;
 
 
-public class HomeFragment extends Fragment {
+public class AccountFragment extends Fragment {
 
 
-    public HomeFragment() {
+    public AccountFragment() {
         // Required empty public constructor
     }
 
@@ -23,7 +22,7 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        return inflater.inflate(R.layout.fragment_account, container, false);
     }
 
 }
